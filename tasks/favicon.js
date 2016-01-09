5cad082337549f19c16209bd26336ffd7a498449
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-01-09 15:00:28 +0100

    v2 proof of concept

--- tasks/favicon.js
+++ tasks/favicon.js
@@ -8,41 +8,41 @@ const config = require('../config');
 
 gulp.task('favicon', () => {
 
-    del(config.favicons.view);
-
-    favicons({
-        files: {
-            src: config.favicons.src,
-            dest: config.favicons.dest,
-            html: config.favicons.view,
-            iconsPath: '/',
-            androidManifest: config.favicons.dest,
-            browserConfig: config.favicons.dest,
-            firefoxManifest: config.favicons.dest,
-            yandexManifest: config.favicons.dest,
-        },
-        icons: {
-            android: true,
-            appleIcon: true,
-            appleStartup: false,
-            coast: false,
-            favicons: true,
-            firefox: true,
-            opengraph: false,
-            windows: true,
-            yandex: false,
-        },
-        config: {
-            appName: config.app.name,
-            appDescription: config.app.description,
-            developer: config.app.developer,
-            developerURL: config.app.developerUrl,
-            version: 1.0,
-            background: '#ffffff',
-            index: '/',
-            url: config.app.url,
-            silhouette: false,
-            logging: false,
-        },
-    })
-})
+    del(config.favicons.view, () => {
+        favicons({
+            files: {
+                src: config.favicons.src,
+                dest: config.favicons.dest,
+                html: config.favicons.view,
+                iconsPath: '/',
+                androidManifest: config.favicons.dest,
+                browserConfig: config.favicons.dest,
+                firefoxManifest: config.favicons.dest,
+                yandexManifest: config.favicons.dest,
+            },
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: false,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                opengraph: false,
+                windows: true,
+                yandex: false,
+            },
+            config: {
+                appName: config.app.name,
+                appDescription: config.app.description,
+                developer: config.app.developer,
+                developerURL: config.app.developerUrl,
+                version: 1.0,
+                background: '#ffffff',
+                index: '/',
+                url: config.app.url,
+                silhouette: false,
+                logging: false,
+            },
+        });
+    });
+});
--- tasks/revision.js
+++ tasks/revision.js
@@ -8,11 +8,11 @@ const config = require('../config');
 
 gulp.task('revision', () => {
 
-    del(config.paths.build);
-
-    return gulp.src([`${config.js.dest}/**/*`, `${config.css.dest}/**/*`], { base: 'public' })
-        .pipe(revision())
-        .pipe(gulp.dest(config.paths.build))
-        .pipe(revision.manifest())
-        .pipe(gulp.dest(config.paths.build))
+    del(config.paths.build, () => {
+        return gulp.src([`${config.js.dest}/**/*`, `${config.css.dest}/**/*`], { base: 'public' })
+            .pipe(revision())
+            .pipe(gulp.dest(config.paths.build))
+            .pipe(revision.manifest())
+            .pipe(gulp.dest(config.paths.build))
+    });
 });
