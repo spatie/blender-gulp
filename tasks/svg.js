16ae314aa3f3b9f4131fc644eb6f6e65f39cdbc4
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-01-15 22:13:04 +0100

    Webpack production config

--- tasks/svg.js
+++ tasks/svg.js
@@ -6,7 +6,7 @@ const svgmin = require('gulp-svgmin');
 const config = require('../config');
 
 gulp.task('svg', () => {
-    
+
     return gulp.src(`${config.svg.src}/**/*.svg`)
         .pipe(svgmin())
         .pipe(gulp.dest(config.svg.dest));
--- tasks/webpack.js
+++ tasks/webpack.js
@@ -2,15 +2,25 @@
 
 const childProcess = require('child_process');
 const gulp = require('gulp');
+const webpack = require('webpack');
 
 const config = require('../config');
+const webpackConfig = require('../config/webpack');
 
 gulp.task('webpack', (callback) => {
 
     const options = [];
 
     if (config.production) {
-        options[] = '-p';
+        options.push('-p');
+
+        webpackConfig.plugins.push([
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        ]);
     }
 
     childProcess.spawn('webpack', options, { stdio: 'inherit' });
