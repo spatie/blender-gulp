227d07802d06ac21cc8e55b5df00f0964ff50cb2
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-01-18 22:38:20 +0100

    I should stop but this is worth it

--- config/webpack.js
+++ config/webpack.js
@@ -3,102 +3,100 @@
 const autoprefixer = require('autoprefixer');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
 const production = process.env.NODE_ENV === 'production';
 
-const configure = (module) => {
+const ExtractFrontCss = new ExtractTextPlugin('css/front.css');
+const ExtractBackCss = new ExtractTextPlugin('css/back.css');
 
-    const config = {
-        context: path.resolve(process.cwd(), 'resources/assets'),
-        output: {
-            path: path.resolve(process.cwd(), 'public/assets'),
-            filename: `${module}/[name].js`,
-            chunkFilename: `${module}/[name].js`,
-            publicPath: '/assets/',
-        },
-        module: {
-            hot: { accept: true },
-            loaders: [
-                {
-                    test: /.jsx?$/,
-                    loaders: ['react-hot', 'babel'],
-                    exclude: /node_modules/,
-                },
-                {
-                    test: /\.scss$/,
-                    loader: ExtractTextPlugin.extract('style', 'css!sass'),
-                },
-                {
-                    test: /\.(png|jpg|jpeg|gif|svg)$/,
-                    loader: 'url?limit=250000',
-                },
-            ],
-        },
-        plugins: [
-            new CleanWebpackPlugin(`public/assets/${module}`, {
-                root: process.cwd(),
-                verbose: false,
-            }),
-            new ExtractTextPlugin(`${module}-site`, `${module}/site.css`, { disable: !production }),
-            function() {
-                this.plugin('watch-run', function(watching, callback) {
-                    console.log('Begin compile at ' + new Date());
-                    callback();
-                });
+const config = {
+    context: path.resolve(process.cwd(), 'resources/assets'),
+    output: {
+        path: path.resolve(process.cwd(), 'public/build'),
+        filename: `js/[name].js`,
+        chunkFilename: `js/[name].js`,
+        publicPath: '/assets/',
+    },
+    module: {
+        hot: { accept: true },
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loaders: ['react-hot', 'babel'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                include: /\/sass\/front\//,
+                loader: ExtractFrontCss.extract('style', ['css', 'sass']),
+            },
+            {
+                test: /\.scss$/,
+                include: /\/sass\/back\//,
+                loader: ExtractBackCss.extract('style', ['css', 'sass']),
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                loader: 'url?limit=250000',
             },
         ],
-        sassLoader: {
-            includePaths: [path.resolve(process.cwd(), 'node_modules')],
+    },
+    plugins: [
+        new ManifestPlugin({
+            fileName: 'rev-manifest.json',
+            basePath: '/build/'
+        }),
+        new CleanWebpackPlugin('public/build', {
+            root: process.cwd(),
+            verbose: false,
+        }),
+        ExtractFrontCss,
+        ExtractBackCss,
+        function() {
+            this.plugin('watch-run', function(watching, callback) {
+                console.log('Begin compile at ' + new Date());
+                callback();
+            });
         },
-        postcss() {
-            return [ autoprefixer ];
-        },
-        resolve: { extensions: ['', '.js', '.jsx', '.css', '.scss'], },
+    ],
+    sassLoader: {
+        includePaths: [path.resolve(process.cwd(), 'node_modules')],
+    },
+    postcss() {
+        return [ autoprefixer ];
+    },
+    resolve: { extensions: ['', '.js', '.jsx', '.css', '.scss'], },
+};
 
-    };
-
-    if (production) {
-        config.plugins = config.plugins.concat([
-            new webpack.optimize.OccurenceOrderPlugin(),
-            new webpack.optimize.UglifyJsPlugin({
-                compress : {
-                    warnings: false,
-                    screw_ie8 : true,
-                },
-                mangle : {
-                    screw_ie8 : true,
-                },
-            }),
-        ]);
-    }
-
-    config.devServer = {
-        port: process.env.WEBPACK_PORT,
-        contentBase: 'public',
-        proxy: {
-            '*': {
-                target: process.env.WEBPACK_PROXY,
-                changeOrigin: true,
-                autoRewrite: true,
-                xfwd: true,
+if (production) {
+    config.plugins = config.plugins.concat([
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress : {
+                warnings: false,
+                screw_ie8 : true,
             },
+            mangle : {
+                screw_ie8 : true,
+            },
+        }),
+    ]);
+}
+
+config.devServer = {
+    port: process.env.WEBPACK_PORT,
+    contentBase: 'public',
+    proxy: {
+        '*': {
+            target: process.env.WEBPACK_PROXY,
+            changeOrigin: true,
+            autoRewrite: true,
+            xfwd: true,
         },
-    };
-
-    return config;
-};
-
-const modules = {
-    front: configure('front'),
-    back: configure('back'),
-};
-
-module.exports = {
-    front: modules.front,
-    back: modules.back,
-    export: () => {
-        return modules[process.env.WEBPACK_MODULE];
     },
 };
+
+module.exports = config;
