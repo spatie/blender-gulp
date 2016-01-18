1cc44ec62f6c4026d0dc7f0128e4bc93f3b0932b
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-01-18 18:06:31 +0100

    Webpack

--- config/webpack.js
+++ config/webpack.js
@@ -1,66 +1,75 @@
 'use strict';
 
-require('dotenv').load();
-
+const autoprefixer = require('autoprefixer');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
-const production = process.env.NODE_ENV === 'production';
-const devServerPort = process.env.WEBPACK_PORT || 3000;
-const devServerProxy = process.env.WEBPACK_PROXY;
+module.exports = (PRODUCTION, MODULE) => {
 
-const config = {
-    context: path.resolve(process.cwd(), 'resources/assets'),
-    output: {
-        path: path.resolve(process.cwd(), 'public'),
-        filename: '[name]',
-        publicPath: `http://localhost:${devServerPort}/`,
-    },
-    module: {
-        hot: {
-            accept: true
+    const config = {
+        context: path.resolve(process.cwd(), 'resources/assets'),
+        output: {
+            path: path.resolve(process.cwd(), 'public/assets'),
+            filename: `${MODULE}/[name].js`,
+            chunkFilename: `${MODULE}/[name].js`,
+            publicPath: '/assets/',
         },
-        loaders: [
-            {
-                test: /\.scss$/,
-                loaders: ['style', 'css', 'sass'],
+        module: {
+            hot: { accept: true },
+            loaders: [
+                {
+                    test: /.jsx?$/,
+                    loaders: ['react-hot', 'babel'],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.scss$/,
+                    loader: ExtractTextPlugin.extract('style', 'css!sass'),
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg)$/,
+                    loader: 'url?limit=250000',
+                },
+            ],
+        },
+        plugins: [
+            new CleanWebpackPlugin('public/assets', {
+                root: process.cwd(),
+                verbose: false,
+            }),
+            new ExtractTextPlugin(`${MODULE}-site`, `${MODULE}/site.css`, { disable: !PRODUCTION }),
+            function() {
+                this.plugin('watch-run', function(watching, callback) {
+                    console.log('Begin compile at ' + new Date());
+                    callback();
+                });
             },
         ],
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx', '.css', '.scss'],
-    },
-    plugins: [],
-    sassLoader: {
-        includePaths: [path.resolve(process.cwd(), 'node_modules')],
-    },
-    devServer: {
-        port: devServerPort,
-        contentBase: 'public',
-        proxy: {
-            '*': {
-                target: devServerProxy,
-                changeOrigin: true,
-                autoRewrite: true,
-                xfwd: true,
-            },
+        sassLoader: {
+            includePaths: [path.resolve(process.cwd(), 'node_modules')],
         },
-    },
+        postcss() {
+            return [ autoprefixer ];
+        },
+        resolve: { extensions: ['', '.js', '.jsx', '.css', '.scss'], },
+    };
+
+    if (PRODUCTION) {
+        config.plugins = config.plugins.concat([
+            new webpack.optimize.OccurenceOrderPlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                compress : {
+                    warnings: false,
+                    screw_ie8 : true,
+                },
+                mangle : {
+                    screw_ie8 : true,
+                },
+            }),
+        ]);
+    }
+
+    return config;
 };
-
-if (production) {
-
-    config.plugins.push(new webpack.optimize.OccurenceOrderPlugin());
-
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-        compress : {
-            warnings: false,
-            screw_ie8 : true,
-        },
-        mangle : {
-            screw_ie8 : true,
-        },
-    }));
-}
-
-module.exports = config;
