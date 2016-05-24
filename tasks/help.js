bc460857d190c408da9b85e251395e9894df4302
Willem Van Bockstal <willem@spatie.be>
2016-05-24 14:46:41 +0200

    Added versions of npm and webpack

--- tasks/help.js
+++ tasks/help.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp');
 const gutil = require('gulp-util');
-const pkg = require('../package.json');
+const versions = require('../lib/versions');
 
 gulp.task('help', () => {
 
     gutil.log('');
-    gutil.log(gutil.colors.white('blender-gulp v' + pkg.version));
+    gutil.log('');
+    gutil.log(gutil.colors.black.bgWhite('Versions'));
+    gutil.log(gutil.colors.white('npm          ' + versions.npm));
+    gutil.log(gutil.colors.white('webpack      ' + versions.webpack));
+    gutil.log(gutil.colors.white('blender-gulp ' + versions.blenderGulp));
     gutil.log('');
     gutil.log(gutil.colors.black.bgCyan('Build tasks'));
     gutil.log(gutil.colors.green('gulp'), gutil.colors.yellow('[--production]'), ' compile [& minify] assets 📦');
