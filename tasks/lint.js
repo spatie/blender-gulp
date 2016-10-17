b57c99c508da31da57b65ab6c521b7b5c9da1696
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-10-17 15:21:07 +0200

    Added js linting

--- tasks/lint.js
+++ tasks/lint.js
@@ -1,9 +1,24 @@
 const gulp = require('gulp');
 const sassLint = require('gulp-sass-lint');
+const spawn = require('../lib/spawn');
 
 const config = require('../config');
 
-gulp.task('lint:sass', function () {
+gulp.task('lint:js', (callback) => {
+    spawn(
+        'node_modules/.bin/eslint',
+        [
+            ...config.lint.js,
+            '--fix',
+            '--ext', '.js',
+            '--ext', '.jsx',
+            '--ext', '.vue',
+        ],
+        callback
+    );
+});
+
+gulp.task('lint:sass', () => {
     gulp.src(config.lint.sass)
         .pipe(sassLint())
         .pipe(sassLint.format())
